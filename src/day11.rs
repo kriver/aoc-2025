dc5431ff3e10bb30a467536733eaa9a2963882c4
Kris Verbeeck <kris.verbeeck@worldline.com>
2025-12-24 14:30:22 +0100

    day 11b

--- src/day11.rs
+++ src/day11.rs
@@ -1,9 +1,12 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    vec,
+};
 
 use crate::util::load;
 
 type Devices = HashMap<String, Vec<String>>;
-type Steps = HashMap<String, (usize, HashSet<String>)>;
+type DeviceLevels = HashMap<String, usize>; // level of a device
 
 pub fn input() -> Devices {
     let lines: Vec<String> = load("data/day11.txt");
@@ -18,28 +21,115 @@ pub fn input() -> Devices {
         .collect()
 }
 
-pub fn part1(devices: Devices) -> usize {
-    let start = "you".to_owned();
-    let mut visited: Steps = HashMap::from([(start.clone(), (1, HashSet::new()))]);
-    let mut todo: Vec<_> = vec![start];
+fn all_paths(devices: &Devices, start: &str, finish: &str, levels: &DeviceLevels) -> usize {
+    let finish_level = levels.get(finish).unwrap();
+    let mut todo: Vec<_> = vec![start.to_string()];
+    let mut cnt = 0;
     loop {
         if todo.is_empty() {
             break;
         }
         let current = todo.pop().unwrap();
+        let current_level = levels.get(&current).unwrap();
+        if current_level >= finish_level {
+            continue;
+        }
         let nexts = devices.get(&current);
         for next in nexts.unwrap_or(&vec![]) {
-            let entry = visited.entry(next.clone()).or_insert((0, HashSet::new()));
-            entry.0 += 1;
-            entry.1.insert(current.clone());
-            todo.push(next.clone());
+            if next == finish {
+                cnt += 1;
+            } else {
+                todo.push(next.clone());
+            }
         }
     }
-    visited["out"].0
+    cnt
 }
 
-pub fn part2(_devices: Devices) -> u64 {
-    0
+fn build_levels(devices: &Devices) -> DeviceLevels {
+    let start = "svr".to_owned();
+    let mut lvl = 0;
+    let mut levels: DeviceLevels = HashMap::from([(start.clone(), lvl)]);
+    let mut todo = vec![start];
+    while !todo.is_empty() {
+        let mut ld_next = HashSet::new();
+        lvl += 1;
+        for device in todo.drain(..) {
+            let next_devices = devices.get(&device);
+            for next in next_devices.unwrap_or(&vec![]) {
+                levels.insert(next.clone(), lvl);
+                ld_next.insert(next.clone());
+            }
+        }
+        todo = ld_next.iter().cloned().collect();
+    }
+    levels
+}
+
+fn all_paths_multi(
+    devices: &Devices,
+    start: Vec<&str>,
+    finish: Vec<&str>,
+    levels: &DeviceLevels,
+    res: Vec<usize>,
+) -> Vec<usize> {
+    finish
+        .into_iter()
+        .map(|f| {
+            start
+                .iter()
+                .enumerate()
+                .map(|(i, s)| res[i] * all_paths(devices, s, f, levels))
+                .sum()
+        })
+        .collect()
+}
+
+pub fn part1(devices: Devices) -> usize {
+    let levels = build_levels(&devices);
+    all_paths(&devices, "you", "out", &levels)
+}
+
+pub fn part2(devices: Devices) -> usize {
+    // Turn data into dot:
+    // gawk '
+    //   BEGIN { FS=":? "; print "digraph "test" {"};
+    //   END {print "}"};
+    //   {for (i=2;i<=NF;i++) {print $1" -> "$i";"}}'
+
+    let levels = build_levels(&devices);
+    let mut res = vec![1];
+    res = all_paths_multi(&devices, vec!["svr"], vec!["fft"], &levels, res);
+    res = all_paths_multi(
+        &devices,
+        vec!["fft"],
+        vec!["amt", "kmc", "fxz", "njx", "opq"],
+        &levels,
+        res,
+    );
+    res = all_paths_multi(
+        &devices,
+        vec!["amt", "kmc", "fxz", "njx", "opq"],
+        vec!["hnh", "zle", "tyl", "vic", "ibk"],
+        &levels,
+        res,
+    );
+    res = all_paths_multi(
+        &devices,
+        vec!["hnh", "zle", "tyl", "vic", "ibk"],
+        vec!["fkd", "xba", "ekg", "mmm" /*"hud"*/],
+        &levels,
+        res,
+    );
+    res = all_paths_multi(
+        &devices,
+        vec!["fkd", "xba", "ekg", "mmm" /*"hud"*/],
+        vec!["dac"],
+        &levels,
+        res,
+    );
+    res = all_paths_multi(&devices, vec!["dac"], vec!["out"], &levels, res);
+    res[0]
 }
 
 #[cfg(test)]
@@ -53,6 +143,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(input()), 0);
+        assert_eq!(part2(input()), 362956369749210);
     }
 }
